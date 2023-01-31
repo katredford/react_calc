9c33d279422ffd997589491a8307c49f84279ef8
katredford <katharine.redford@gmail.com>
2023-01-31 11:54:13 -0600

    now can increment and decrement with useReducer

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@
 import './App.css';
 import React, {useState, useReducer} from "react"
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 }
+    case "decrement":
+      return { count: state.count - 1 }
+    default:
+      return state
+  }
+  
+}
+
 export function App() {
-  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, {count: 0})
+  // const [count, setCount] = useState(0)
 
   function increment() {
-    setCount( prevCount => prevCount + 1)
+    // setCount( prevCount => prevCount + 1)
+    dispatch({ type: "increment"})
   }
 
   function decrement() {
-    setCount(prevCount => prevCount - 1)
+    // setCount(prevCount => prevCount - 1)
+    dispatch({type: "decrement"})
   }
   return (
     <div >
       <button onClick={decrement}> - </button>
-      <span>{count}</span>
+      <span>{state.count}</span>
       <button onClick={increment}> + </button>
     </div>
   );
