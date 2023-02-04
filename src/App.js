8c39080f9a4c1a73c39b6cd962c024c391a90d14
katredford <katharine.redford@gmail.com>
2023-02-03 19:12:03 -0600

    finish reducer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import React, { useState, useReducer } from "react"
 import Todo from "./components/Todo/index.js"
 
-const ACTIONS = {
+export const ACTIONS = {
   ADD_TODO: "add-todo",
-  TOGGLE_TODO: "toggle-todo"
+  TOGGLE_TODO: "toggle-todo",
+  DELETE_TODO: "delete-todo"
 }
 
 function reducer(todos, action) {
@@ -13,7 +14,16 @@ function reducer(todos, action) {
     case ACTIONS.ADD_TODO:
       return [...todos, newTodo(action.payload.name)]
     case ACTIONS.TOGGLE_TODO:
-      return todos.map
+      return todos.map(todo => {
+        if (todo.id === action.payload.id) {
+          return {...todo, complete: !todo.complete}
+        }
+        return todo
+      })
+    case ACTIONS.DELETE_TODO:
+      return todos.filter(todo => todo.id !== action.payload.id)
+    default:
+      return todos
       
     // default:
     //   return todos
@@ -46,7 +56,7 @@ export function App() {
 onChange={e => setName(e.target.value)} />
       </form>
       {todos.map(todo => {
-        return <Todo key={todo.id} todo={todo} />
+        return <Todo key={todo.id} todo={todo} dispatch={dispatch} />
       })}
     </>
   );
--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,16 +1,22 @@
 
 import React from "react"
-
-export default function Todo({todo}) {
+import { ACTIONS } from '../../App'
+export default function Todo({todo, dispatch}) {
 
   return (
     <>
-      <span>
-        {/* style={{color: todo.complete ? '#AAA' : '#000'}} */}
+      <span style={{color: todo.complete ? '#AAA' : '#000'}}>
         {todo.name}
       </span>
-      <button>Toggle</button>
-      <button>Delete</button>
+      <button onClick={() => dispatch({
+        type: ACTIONS.TOGGLE_TODO,
+        payload: { id: todo.id }
+      })}>Toggle</button>
+
+      <button onClick={() => dispatch({
+        type: ACTIONS.DELETE_TODO,
+        payload: { id: todo.id }
+          })}>Delete</button>
     </>
   )
 }
